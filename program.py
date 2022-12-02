9679c02320dbc13ed78ca85e944e2a5fa8bc05b8
AlexanderSychev2005 <alexandersychev2005@gmail.com>
2022-12-02 10:35:30 +0200

    add running line

--- program.py
+++ program.py
@@ -1,5 +1,16 @@
+import time
 n=9
 fact=1
 for i in range(2,n+1): 
   fact=fact*i 
 print("The factorial of ",n," is: ",fact)
+
+
+
+
+
+stroka = input("")
+
+for slovo in stroka:
+    print(slovo,end="  ", flush=True)
+    time.sleep(1)
